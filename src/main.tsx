e0d04483775d193c8b441095b8935366063f86f6
Gessé Carlos <gesse.dev@outlook.com>
2022-02-02 03:36:48 -0300

    removed unnecessary browser router on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store } from './redux/store';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <Provider store={ store }>
-      <App />
-    </Provider>
-  </Router>,
+  <Provider store={ store }>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );

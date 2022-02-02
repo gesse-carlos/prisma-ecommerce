6021774bb644728e0fa4a671262361dd4827feea
Gessé Carlos <gesse.dev@outlook.com>
2022-02-02 02:34:56 -0300

    inserted cartReducer on store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { cartSlice } from './reducers/cart';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartSlice.reducer,
+  },
 });
 
 
